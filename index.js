84e8e159aace7c8723793cf40df7e511cf612750
NattyNgrn <natnrgn@gmail.com>
2023-08-24 21:00:54 -0400

    functions for CRUD

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const express = require("express");
 const app = express();
 const port = 4444;
 
-function meow(req, res) {
+function getfunc(req, res) {
     const musicJson = require("./music.json");
     //const music = JSON.parse(musicJson);
     console.log(musicJson.music[0]);
-
 }
-meow();
-app.get('/', meow)
 
-app.listen(port, () => console.log(`ur mom is listening at ${port}`))
+function putfunc(req, res){
+    const musicJson = require("./music.json");
+    console.log(musicJson.music);
+}
+
+function postfunc(req, res){
+    const musicJson = require("./music.json");
+    console.log(musicJson.music);
+}
+
+function deletefunc(req, res){
+    const musicJson = require("./music.json");
+    console.log(musicJson.music);
+}
+
+app.get('/', getfunc);
+app.put('/', putfunc);
+app.post('/', postfunc);
+app.delete('/', deletefunc);
+app.listen(port, () => console.log(`ur mom is listening at ${port}`));
