59e21399cabb99a4bf4eaa523b751a6560d4bf7d
NattyNgrn <natnrgn@gmail.com>
2023-08-24 20:22:47 -0400

    express and ports

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 4444;
+
+function meow(req, res) {
+    res.send("hullo");
+}
+app.get('/', meow)
+
+app.listen(port, () => console.log(`ur mom is listening at ${port}`))
